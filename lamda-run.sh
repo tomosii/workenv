2194a94a1a0688698eb720d2426aaa8afd8970f1
tomosii <lighthouse.t17@gmail.com>
2026-01-28 10:36:21 +0000

    Add wandb

--- lamda-run.sh
+++ lamda-run.sh
@@ -9,6 +9,10 @@ cd ~/
 git clone git@github.com:tomosii/hierarchical-wm.git
 
 
+mkdir -p ~/hierarchical-wm/src/sunta/data/
+cp -r -u ~/iiyama/dataset/serial_nine_rooms ~/hierarchical-wm/src/sunta/data/
+
+
 
 # docker run -t -d --rm \
 #     --gpus all \
@@ -20,14 +24,16 @@ git clone git@github.com:tomosii/hierarchical-wm.git
 
 
 if [ "$(sudo docker ps -q -f name=$CONTAINER_NAME)" ]; then
-    sudo docker exec -t $CONTAINER_NAME bash /workspace/workenv/train.sh
+    sudo docker exec -t -d -e WANDB_API_KEY=$WANDB_API_KEY $CONTAINER_NAME bash /workspace/workenv/train.sh
 else
     # sudo docker rm -f $CONTAINER_NAME || true
+    # sudo docker run -it --rm \
     sudo docker run -t -d --rm \
         --gpus all \
         -v ~/:/workspace \
         -e WANDB_API_KEY=$WANDB_API_KEY \
         --name $CONTAINER_NAME $IMAGE_NAME \
         bash /workspace/workenv/train.sh
+        # bash
 fi
 
--- train.sh
+++ train.sh
@@ -19,25 +19,46 @@ cd hierarchical-wm/src/sunta
     # seed=0
 
 
-python3 train.py \
-    dataset=serial_nine_rooms \
-    model=sunta \
-    train_level=1 \
-    l1_deter_size=512 \
-    l1_stoch_size=16 \
-    total_steps=500000 \
-    train_sequence_length=200 \
-    test_sequence_length=200 \
-    context_length=50 \
-    test_every=50000 \
-    save_checkpoint_every=20000 \
-    seed=0
-
-
-
-
-
-
+# python3 train.py \
+#     dataset=serial_nine_rooms \
+#     model=sunta \
+#     train_level=1 \
+#     l1_deter_size=512 \
+#     l1_stoch_size=32 \
+#     total_steps=500000 \
+#     train_sequence_length=344 \
+#     test_sequence_length=344 \
+#     context_length=50 \
+#     test_every=50000 \
+#     save_checkpoint_every=20000 \
+#     seed=0 \
+    # decoder_input=state_feature \
+    # test_every=1 \
 
 # python3 eval.py eval_run_path=lighthouse117/sunta/jytaliqh
 
+
+python3 train.py \
+    dataset=serial_nine_rooms \
+    model=sunta \
+    train_level=2 \
+    level1_run_path=lighthouse117/sunta/k0rbkx53 \
+    l1_deter_size=128 \
+    l1_stoch_size=8 \
+    l2_deter_size=128 \
+    l2_stoch_size=8 \
+    total_steps=500000 \
+    train_sequence_length=200 \
+    test_sequence_length=200 \
+    context_length=100 \
+    test_every=20000 \
+    save_checkpoint_every=20000 \
+    seed=0 \
+    pattern_completion_loss=True \
+    loss_scales.pattern_completion_kl=1.0 \
+    deter_zero_init=False \
+    l2_decoder_independent=False \
+    action_conditioned_level2=False \
+    contrastive_negative_loss=True \
+    contrastive_negative_margin=50.0 \
+    boundary_peak_prominence=5.0 \
